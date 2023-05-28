c55d350600c8914c3c49744dd651b8a3417bd440
Murali <123637911+muralisiva8896@users.noreply.github.com>
2023-05-28 12:22:22 +0530

    Add files via upload

---
+++ CucumberCrashCourse/src/test/java/Runner/TestRunner.java
@@ -0,0 +1,20 @@
+package Runner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.junit.runner.RunWith;
+import org.testng.annotations.DataProvider;
+
+
+@CucumberOptions(features = "src/test/ResourceFiles/featurefiles",
+        glue = {"com.mystepdefs"},
+        plugin = {"pretty", "html:target/cucumber-reports", "json:target/cucumber.json"},
+        monochrome = true,
+        publish = true)
+public class TestRunner extends AbstractTestNGCucumberTests {
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
+}
